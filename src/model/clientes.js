7f1fbf36a8159043a9ae31742e8dee5246968340
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-02-18 17:07:17 -0300

    feature: db model change

--- src/model/clientes.js
+++ src/model/clientes.js
@@ -10,7 +10,7 @@ export const Clientes = connection.define(
       allowNull: false,
       primaryKey: true,
     },
-    nome: {
+    email: {
       type: Sequelize.STRING,
       allowNull: false,
     },
